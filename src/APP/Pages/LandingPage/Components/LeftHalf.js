8ee59ecbfadaadaafef081da2579f2dd88efe16c
gich2009 <waneneamos@gmail.com>
2023-06-05 19:52:44 +0300

    SS-5 Added the button functionalities.

--- src/APP/Pages/LandingPage/Components/LeftHalf.js
+++ src/APP/Pages/LandingPage/Components/LeftHalf.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../style.css";
 
 function LeftHalf(){
+  const navigate = useNavigate();
+
+  function handleSignup(){
+    navigate("/signup");  
+  }
+
+  function handleLogin(){
+    navigate("/login");
+  }
+
   return(
     <div className="LeftHalf">
       <h1>Simple Store</h1>
@@ -10,8 +20,8 @@ function LeftHalf(){
       <h3>Sign up Now and check us out for free</h3>
       <Link to="/home" className="LeftHalfLink">Learn More</Link>
       <div>
-        <button>SIGN UP</button>
-        <button>LOG IN</button>
+        <button onClick={handleSignup}>SIGN UP</button>
+        <button onClick={handleLogin}>LOG IN</button>
       </div>
     </div>
   )
