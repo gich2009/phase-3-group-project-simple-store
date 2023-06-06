c1e7b242f33621dea543e4cdeccb9997e4203350
fartun <fartunwario34@gmail.com>
2023-06-07 01:31:40 +0300

    SS-16 completed

--- src/APP/Pages/ProfilePage/ListedItemSection/index.js
+++ src/APP/Pages/ProfilePage/ListedItemSection/index.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-function ListItemSection({ sectionTitle, items }) {
+function ListItemSection({ sectionTitle }) {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const items = [
+    { id: 1, name: "Item 1", stock: 10, price: 9.99, image: "item1.jpg" },
+    { id: 2, name: "Item 2", stock: 5, price: 19.99, image: "item2.jpg" },
+    // Add more items as needed
+  ];
+
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
   return (
-    <div>
+    <div className="listed-item-section">
       <h2>{sectionTitle}</h2>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
+      <div className="listed-items">
+        {items && items.map((item) => (
+          <div
+            className={`item-card${selectedItem === item ? " active" : ""}`}
+            key={item.id}
+            onClick={() => handleItemClick(item)}
+          >
+            <img src={`/images/${item.image}`} alt={item.name} />
+            <div className="item-info">
+              <h3>{item.name}</h3>
+              <p>Stock: {item.stock}</p>
+              <p>Price: {item.price}</p>
+              <div className="button-card">
+                <button className="sell-button">Sell</button>
+                <button className="restock-button">Restocking</button>
+                <button className="update-button">Update</button>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
