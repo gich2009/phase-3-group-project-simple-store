271b7a5e7ecbca4515dc54d8bddbd1a710a91ea2
gich2009 <waneneamos@gmail.com>
2023-06-06 19:05:44 +0300

    SS-7 Initial commit

--- src/APP/Pages/LoginPage/index.js
+++ src/APP/Pages/LoginPage/index.js
@@ -1,11 +1,36 @@
-import React from "react";
+import { React, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./style.css";
 
-function LoginPage(){
+function LoginPage() {
+  const [username, setUsername]               = useState('');
+  const [password, setPassword]               = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // handle form submission here
+    navigate("/home/create");
+  }
+
+  function handleCancel(){
+    navigate("/");
+  }
+
   return (
-    <div>
-      <h1>This is the login page</h1>
+    <div className="signup-form-container">
+      <form onSubmit={handleSubmit} className="signup-form">
+        <h1>Log In</h1>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} required placeholder="Enter username" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} required placeholder="Enter password" />
+        <button type="submit">Log in</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </form>
     </div>
   );
+  
 }
 
 export default LoginPage;
