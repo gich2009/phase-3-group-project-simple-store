2a68240012d08f3483eaef2e3ea6bb864d1f1a15
gich2009 <waneneamos@gmail.com>
2023-06-06 15:44:26 +0300

    SS-11 Finished the metrics section

--- src/APP/Pages/HomePage/MetricSection/index.js
+++ src/APP/Pages/HomePage/MetricSection/index.js
@@ -1,9 +1,13 @@
 import React from "react"
+import MetricList from "./Components/MetricList";
+import "./style.css";
 
 function MetricSection(){
   return (
-    <div>
-      <h1>This is the <strong>metrics section</strong> of the home page</h1>
+    <div className="metric-section">
+      <h1>Our Metrics</h1>
+      <h1>Here are some of the key numbers from out previous years of operations</h1>
+      <MetricList />
     </div>
   );
 }
