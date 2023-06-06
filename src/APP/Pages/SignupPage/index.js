81756030f1236204d6e5c43b54ddffc27e149961
gich2009 <waneneamos@gmail.com>
2023-06-06 18:48:53 +0300

    SS-6 Finished creating the sign up pages

--- src/APP/Pages/SignupPage/index.js
+++ src/APP/Pages/SignupPage/index.js
@@ -1,9 +1,42 @@
-import React from "react";
+import { React, useState }from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 function SignupPage(){
+  const [username, setUsername]               = useState('');
+  const [email, setEmail]                     = useState('');
+  const [password, setPassword]               = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [rememberMe, setRememberMe]           = useState(false);
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // handle form submission here
+    navigate("/home");
+  }
+
+  function handleCancel(){
+    navigate("/");
+  }
+
   return (
-    <div>
-      <h1>This is the signup page</h1>
+    <div className="signup-form-container">
+      <form onSubmit={handleSubmit} className="signup-form">
+        <h1>Sign Up</h1>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} required placeholder="Enter username" />
+        <label htmlFor="email">Email Address:</label>
+        <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} required placeholder="Enter email address" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} required placeholder="Enter password" />
+        <label htmlFor="confirm-password">Confirm Password:</label>
+        <input type="password" id="confirm-password" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} required placeholder="Confirm password" />
+        <input type="checkbox" id="remember-me" checked={rememberMe} onChange={(event) => setRememberMe(event.target.checked)} />
+        <label htmlFor="remember-me">Remember me</label>
+        <button type="submit">Sign Up</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </form>
     </div>
   );
 }
