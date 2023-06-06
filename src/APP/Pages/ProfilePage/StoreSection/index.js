b2341465d2c0035ac104adf808bbb0348a01f3eb
Jerome Otieno <sakajerome11@gmail.com>
2023-06-07 00:30:49 +0300

    SS-14 <finished task>

--- src/APP/Pages/ProfilePage/StoreSection/index.js
+++ src/APP/Pages/ProfilePage/StoreSection/index.js
@@ -1,16 +1,3 @@
-import React from "react";
-import HeaderSection from "../HeaderSection";
-
-function StoreSection(){
-  return (
-    <div>
-      <h1>This is the <strong>store section</strong> of the profile page</h1>
-    </div>
-  );
-}
-
-export default StoreSection;
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
@@ -51,4 +38,4 @@ const Store = () => {
   );
 };
 
-ReactDOM.render(<Store />, document.getElementById('root'));
+export default Store;
