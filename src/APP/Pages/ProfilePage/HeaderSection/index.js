f6e646b2f7765a7866865d16334f9bf823a1e32a
gich2009 <waneneamos@gmail.com>
2023-06-07 15:32:26 +0300

    SS-16 resolving header section of the profile page.

--- src/APP/Pages/ProfilePage/HeaderSection/index.js
+++ src/APP/Pages/ProfilePage/HeaderSection/index.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import icon from "./images/vn_passport.jpg";
 
-function HeaderSection() {
+
+function HeaderSection({ username = "Fartun" }) {
   return (
-    <div>
-      <h1>This is the header section of the profile page</h1>
-    </div>
+    <header>
+      <div className="header-container">
+        <div className="left-section">
+          <h1>Simple Store</h1>
+        </div>
+        <div className="right-section">
+          <ul>
+            <Link className="header-link" to="/home">Home</Link>
+            <Link className="header-link" to="/profile">Profile</Link>
+            <Link className="header-link" to="/">Logout</Link>
+          </ul>
+        </div>
+      </div>
+      <div className="content-section">
+        <div className="frame">
+          <div className="profile-image-container">
+            <img
+              src={icon}
+              alt="Passport"
+              className="profile-image"
+            />
+          </div>
+          <div className="welcome-message">
+            <h2>Hi, {username} !</h2>
+            <p>Welcome to Simple Store</p>
+          </div>
+        </div>
+      </div>
+    </header>
   );
-}
+};
 
 export default HeaderSection;
