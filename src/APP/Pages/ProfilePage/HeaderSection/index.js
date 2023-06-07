07f3e6b9fc9ae7eb7d0524d59fb0465d2fec9e08
gich2009 <waneneamos@gmail.com>
2023-06-07 15:23:16 +0300

    SS-16 Listed item section

--- src/APP/Pages/ProfilePage/HeaderSection/index.js
+++ src/APP/Pages/ProfilePage/HeaderSection/index.js
@@ -1,39 +1,10 @@
 import React from "react";
-// import "./style.css";
-// import passportImage from "./images/vn_passport.jpg";
 
-function HeaderSection({ username }) {
+function HeaderSection() {
   return (
-    // <header>
-    //   <div className="header-container">
-    //     <div className="left-section">
-    //       <h1>Simple Store</h1>
-    //     </div>
-    //     <div className="right-section">
-    //       <ul>
-    //         <li>About</li>
-    //         <li>Profile</li>
-    //         <li>Log Out</li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    //   <div className="content-section">
-    //     <div className="frame">
-    //       <div className="profile-image-container">
-    //         <img
-    //           src={passportImage}
-    //           alt="Passport"
-    //           className="profile-image"
-    //         />
-    //       </div>
-    //       <div className="welcome-message">
-    //         <h2>Hi, {username}!</h2>
-    //         <p>Welcome to Simple Store</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </header>
-    <></>
+    <div>
+      <h1>This is the header section of the profile page</h1>
+    </div>
   );
 }
 
