d98b4f501f2967c6042906d0dafaf3198be470a4
gich2009 <waneneamos@gmail.com>
2023-06-08 20:31:53 +0300

    SS-15 Rendered the products section of the profile page

--- src/APP/Pages/ProfilePage/ProductSection/index.js
+++ src/APP/Pages/ProfilePage/ProductSection/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import HeaderSection from "../HeaderSection";
 import './style.css';
-import logo from './images/logo.png';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import logo from './images/logo.png';
 import chair1 from './images/chair1.jpeg';
 import chair2 from './images/chair2.jpeg';
 import chair3 from './images/chair3.jpeg';
 import chair4 from './images/chair4.jpeg';
 function ProductSection() {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    fetch('http://localhost:9292/stores/1/products')
+    .then((response) => response.json())
+    .then((data) => setProducts(data));
+  }, []);
+
+
+  function handleListedItem(id){
+    navigate(`products/item/${id}`);
+  }
+
   return (
     <>
       <div className="main-card">
@@ -14,31 +30,53 @@ function ProductSection() {
         <div className="products-section">
           <h3 className="products-header">Products</h3>
           <div className="products-container">
-            <div className="product-card">
-              <img src={chair1} alt="Product 1" />
-              <h4>Womb Chair</h4>
-              <p>Price: KES 1000</p>
-            </div>
-            <div className="product-card">
-              <img src={chair2} alt="Product 2" />
-              <h4>Ball Chair</h4>
-              <p>Price: KES 1500</p>
-            </div>
-            <div className="product-card">
-              <img src={chair3} alt="Product 3" />
-              <h4>Wing Chair</h4>
-              <p>Price: KES 800</p>
-            </div>
-            <div className="product-card">
-              <img src={chair4} alt="Product 4" />
-              <h4>High chair</h4>
-              <p>Price: KES 1200</p>
-            </div>
+            {products.map((product) => (
+              <div className="product-card" key={product.id}  onClick={() => handleListedItem(product.id)}>
+                <h4>{product.name}</h4>
+                <p>Buying Price: KES {product.buying_price}</p>
+                <p>Selling Price: KES {product.selling_price}</p>
+                <p>Stock: {product.stock}</p>
+                <p>Sales: {product.sales}</p>
+                <p>Store ID: {product.store_id}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </>
   );
+  // return (
+  //   <>
+  //     <div className="main-card">
+
+  //       <div className="products-section">
+  //         <h3 className="products-header">Products</h3>
+  //         <div className="products-container">
+  //           <div className="product-card">
+  //             <img src={chair1} alt="Product 1" />
+  //             <h4>Womb Chair</h4>
+  //             <p>Price: KES 1000</p>
+  //           </div>
+  //           <div className="product-card">
+  //             <img src={chair2} alt="Product 2" />
+  //             <h4>Ball Chair</h4>
+  //             <p>Price: KES 1500</p>
+  //           </div>
+  //           <div className="product-card">
+  //             <img src={chair3} alt="Product 3" />
+  //             <h4>Wing Chair</h4>
+  //             <p>Price: KES 800</p>
+  //           </div>
+  //           <div className="product-card">
+  //             <img src={chair4} alt="Product 4" />
+  //             <h4>High chair</h4>
+  //             <p>Price: KES 1200</p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 
